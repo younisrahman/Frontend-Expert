bf62c9d2fa39b25b6341c26d2b5d8b5c255b646a
Younis Rahman <younisrahman007@gmail.com>
2022-12-29 14:30:09 +0600

    Event-Driven Programming

---
+++ React/Event Driven/index.jsx
@@ -0,0 +1,9 @@
+// Welcome to our Playground!
+
+function App() {
+  return <button onClick={handleClick}>Click Me</button>;
+}
+
+function handleClick() {
+  document.body.style.backgroundColor = 'red';
+}
