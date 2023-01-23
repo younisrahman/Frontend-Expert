d6e70545b196afbd1dde33f9952fed17b39c60d4
Younis Rahman <younisrahman007@gmail.com>
2023-01-24 02:03:41 +0600

    chore: changes in my page

--- JS/connectingJS/DOMMainpulation/domManipulation.js
+++ JS/connectingJS/DOMMainpulation/domManipulation.js
@@ -1,8 +1,13 @@
-const secondLi = document.getElementById('second-li');
-const firstLi = document.querySelector('li');
+// const secondLi = document.getElementById('second-li');
+// const firstLi = document.querySelector('li');
 const allLi = document.querySelectorAll('li');
-const bycCass = document.getElementsByClassName('list-item');
-const byTag = document.getElementsByTagName('ol');
-console.log(firstLi);
-console.log(secondLi);
-console.log(allLi);
+// const bycCass = document.getElementsByClassName('list-item');
+// const byTag = document.getElementsByTagName('ol');
+// console.log(firstLi);
+// console.log(secondLi);
+// console.log(allLi);
+
+allLi[0].textContent = 'Changed';
+allLi[0].value = 10;
+allLi[0].setAttribute('value', 11);
+allLi[0].setAttribute('class', 'blue');
