14005b248f1c187b871429b003d438bcc91157fb
Younis Rahman <younisrahman007@gmail.com>
2023-01-23 18:21:15 +0600

    DOM Mainpulation

---
+++ JS/connectingJS/DOMMainpulation/domManipulation.js
@@ -0,0 +1,6 @@
+const secondLi = document.getElementById('second-li');
+const firstLi = document.querySelector('li');
+const allLi = document.querySelectorAll('li');
+console.log(firstLi);
+console.log(secondLi);
+console.log(allLi);
