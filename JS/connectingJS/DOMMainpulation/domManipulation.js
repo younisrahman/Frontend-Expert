b1dc41d6107fc907684861a6147117b3982dd3ff
Younis Rahman <younisrahman007@gmail.com>
2023-01-24 01:58:12 +0600

    chore: changes in my page

--- JS/connectingJS/DOMMainpulation/domManipulation.js
+++ JS/connectingJS/DOMMainpulation/domManipulation.js
@@ -1,6 +1,8 @@
 const secondLi = document.getElementById('second-li');
 const firstLi = document.querySelector('li');
 const allLi = document.querySelectorAll('li');
+const bycCass = document.getElementsByClassName('list-item');
+const byTag = document.getElementsByTagName('ol');
 console.log(firstLi);
 console.log(secondLi);
 console.log(allLi);
