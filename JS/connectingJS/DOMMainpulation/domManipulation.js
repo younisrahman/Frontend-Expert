f523695f7d46c4cdecb1abeba3ad7349d0ede5bb
Younis Rahman <younisrahman007@gmail.com>
2023-01-24 15:21:46 +0600

    chore: changes in my page

--- JS/connectingJS/DOMMainpulation/domManipulation.js
+++ JS/connectingJS/DOMMainpulation/domManipulation.js
@@ -11,7 +11,9 @@ allLi[0].textContent = 'Changed';
 allLi[0].value = 10;
 allLi[0].setAttribute('value', 11);
 allLi[0].setAttribute('class', 'blue big');
+allLi[0].classList.remove('blue');
 allLi[0].classList.add('blue');
+allLi[0].classList.toggle('big');
 
 const p = document.createElement('p');
 p.textContent = 'Hello World';
@@ -21,3 +23,22 @@ document.body.append(p, 'hello');
 document.body.prepend(p, 'hello');
 
 document.body.innerHTML += '<p>HTML Test String</p>'; //not use super buggy
+
+for (let i = 0; i < 3; i++) {
+  const parent = document.querySelector('ol');
+  const li = document.createElement('li');
+  li.textContent = `List item with i=${i}`;
+  parent.append(li);
+}
+
+const parent = document.querySelector('ol');
+const fragment = document.createDocumentFragment();
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement('li');
+  li.textContent = `List item with i=${i}`;
+  fragment.append(li);
+}
+parent.append(fragment);
+
+allLi[0].classList.add('big');
+console.log(window.getComputedStyle(allLi[0]).fontSize);
