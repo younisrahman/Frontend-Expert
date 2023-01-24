a66f805af20778a98f621bece6581250387a5c7a
Younis Rahman <younisrahman007@gmail.com>
2023-01-24 14:00:34 +0600

    chore: changes in my page

--- JS/connectingJS/DOMMainpulation/domManipulation.js
+++ JS/connectingJS/DOMMainpulation/domManipulation.js
@@ -12,3 +12,12 @@ allLi[0].value = 10;
 allLi[0].setAttribute('value', 11);
 allLi[0].setAttribute('class', 'blue big');
 allLi[0].classList.add('blue');
+
+const p = document.createElement('p');
+p.textContent = 'Hello World';
+document.body.appendChild(p);
+document.body.appendChild(p, 'hello');
+document.body.append(p, 'hello');
+document.body.prepend(p, 'hello');
+
+document.body.innerHTML += '<p>HTML Test String</p>'; //not use super buggy
