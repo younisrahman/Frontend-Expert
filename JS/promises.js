05c7050c93e05032127dc40aad193e5be57afa7e
Younis Rahman <younisrahman007@gmail.com>
2023-08-09 02:29:40 +0600

    solve

--- JS/promises.js
+++ JS/promises.js
@@ -1,26 +1,118 @@
+const STATE = {
+  PENDING: "pending",
+  FULFILLED: "fulfilled",
+  REJECTED: "rejected",
+};
 class MyPromise {
-  // Write your code here.
+  #state = STATE.PENDING;
+  #value = null;
+  #fulfilledCallbacks = [];
+  #rejectedCallbacks = [];
 
   constructor(executorFunc) {
-    // Write your code here.
+    try {
+      executorFunc(
+        (value) => this.#resolve(value),
+        (value) => this.#reject(value)
+      );
+    } catch (error) {
+      this.#reject(error);
+    }
   }
 
   then(onFulfilled, onRejected) {
-    // Write your code here.
+    return new MyPromise((resolve, reject) => {
+      const fulfilledCallback = () => {
+        if (!onFulfilled) return resolve(this.#value);
+
+        queueMicrotask(() => {
+          try {
+            const value = onFulfilled(this.#value);
+            resolve(value);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      };
+      const rejectedCallback = () => {
+        if (!onRejected) return reject(this.#value);
+
+        queueMicrotask(() => {
+          try {
+            const value = onRejected(this.#value);
+            resolve(value);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      };
+
+      switch (this.#state) {
+        case STATE.PENDING:
+          this.#fulfilledCallbacks.push(fulfilledCallback);
+          this.#rejectedCallbacks.push(rejectedCallback);
+          break;
+        case STATE.FULFILLED:
+          fulfilledCallback();
+          break;
+        case STATE.REJECTED:
+          rejectedCallback();
+          break;
+        default:
+          throw new Error("Unexpected promise state");
+      }
+    });
   }
 
   catch(onRejected) {
-    // Write your code here.
+    return this.then(null, onRejected);
   }
 
   get state() {
-    // Write your code here.
+    return this.#state;
   }
 
   get value() {
-    // Write your code here.
+    return this.#value;
+  }
+
+  #resolve(value) {
+    this.#value = value;
+    this.#state = STATE.FULFILLED;
+    this.#fulfilledCallbacks.forEach((callback) => callback());
+  }
+
+  #reject(value) {
+    this.#value = value;
+    this.#state = STATE.REJECTED;
+    this.#rejectedCallbacks.forEach((callback) => callback());
   }
 }
 
-// Do not edit the line below.
-exports.MyPromise = MyPromise;
+// new MyPromise((res, rej) => {
+//   setTimeout(() => res(10), 1000);
+// })
+//   .then((value) => {
+//     console.log(value * 2);
+//     return value * 2;
+//   })
+//   .then((value) => {
+//     console.log(value * 3);
+//     return value * 3;
+//   });
+
+const promise = new MyPromise((res, rej) => {
+  setTimeout(() => res(10), 1000);
+});
+
+promise.then((value) => {
+  console.log(value * 2);
+  return value * 2;
+});
+
+promise.then((value) => {
+  console.log(value * 3);
+  return value * 3;
+});
+
+console.log("end");
