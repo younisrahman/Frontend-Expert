ea14b35918e2b0359465803500139d6bd1c3f8e6
Younis Rahman <younisrahman007@gmail.com>
2023-08-09 01:57:59 +0600

    promises

---
+++ JS/promises.js
@@ -0,0 +1,26 @@
+class MyPromise {
+  // Write your code here.
+
+  constructor(executorFunc) {
+    // Write your code here.
+  }
+
+  then(onFulfilled, onRejected) {
+    // Write your code here.
+  }
+
+  catch(onRejected) {
+    // Write your code here.
+  }
+
+  get state() {
+    // Write your code here.
+  }
+
+  get value() {
+    // Write your code here.
+  }
+}
+
+// Do not edit the line below.
+exports.MyPromise = MyPromise;
