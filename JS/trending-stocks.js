c7645d3bad372baee7a381d7eff6836ecc02fb1c
Younis Rahman <younisrahman007@gmail.com>
2023-08-15 01:30:18 +0600

    trending-stocks

---
+++ JS/trending-stocks.js
@@ -0,0 +1,9 @@
+const SYMBOLS_API_BASE_URL =
+  "https://api.frontendexpert.io/api/fe/stock-symbols";
+const MARKET_CAPS_API_BASE_URL =
+  "https://api.frontendexpert.io/api/fe/stock-market-caps";
+const PRICES_API_BASE_URL = "https://api.frontendexpert.io/api/fe/stock-prices";
+
+async function trendingStocks(n) {
+  // Write your code here.
+}
